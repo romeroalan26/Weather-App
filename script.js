c4c51a6fdef350a4621c0fb8ebbf53dfe3876338
Alan Romero <prueba@usuario.com>
2023-05-17 21:55:00 -0400

    update

--- script.js
+++ script.js
@@ -48,9 +48,9 @@ const getWeather = function () {
 								>
 									<h1
 										class="display-4 mb-0 font-weight-bold"
-										style="color: #1c2331"
+										style="color: #1c2331; font-size: 80px"
 									>
-										<b>${response.current.temp_c}°C</b>
+										<b>${response.current.temp_c}°c</b>
 									</h1>
 									<span class="small" style="color: black"
 										>${response.current.condition.text}</span
